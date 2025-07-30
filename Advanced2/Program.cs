d343b9852ee0e9654b30506e38afe6e88735358e
moneka <monekawilson16@gmail.com>
2025-07-30 17:43:15 +0300

    Q2

--- Advanced2/Program.cs
+++ Advanced2/Program.cs
@@ -7,25 +7,37 @@ namespace Advanced2
         static void Main(string[] args)
         {
             #region Q1
-            String[] fristLine = Console.ReadLine().Split();
-            int n = int.Parse(fristLine[0]);
-            int q = int.Parse(fristLine[1]);
+            /*      String[] fristLine = Console.ReadLine().Split();
+                  int n = int.Parse(fristLine[0]);
+                  int q = int.Parse(fristLine[1]);
 
-            int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-            for (int i = 0; i < q; i++) { 
-            int x = int.Parse(Console.ReadLine());
-                int count = 0;
-                foreach (int num in arr) {
-                    if (num > x)
-                    {
-                        count++;
-                    }
-                }
-                Console.WriteLine(count);
-            }
+                  int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                  for (int i = 0; i < q; i++) { 
+                  int x = int.Parse(Console.ReadLine());
+                      int count = 0;
+                      foreach (int num in arr) {
+                          if (num > x)
+                          {
+                              count++;
+                          }
+                      }
+                      Console.WriteLine(count);*/
             #endregion
             #region Q2
+            int n = int.Parse(Console.ReadLine());
+            String[] input = Console.ReadLine().Split();
+            int[] arr = Array.ConvertAll(input, int.Parse);
+            bool isPalindrome = true;
+            for (int i = 0; i < n / 2; i++)
+            {
+                if (arr[i] != arr[n - 1 - i])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            Console.WriteLine(isPalindrome ? "Yes" : "No");
             #endregion
         }
     }
-}
+    }

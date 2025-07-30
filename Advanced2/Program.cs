6de725fa6c6dd0912f36d6160c298162e5bd0a97
moneka <monekawilson16@gmail.com>
2025-07-30 16:28:24 +0300

    Q1

--- Advanced2/Program.cs
+++ Advanced2/Program.cs
@@ -6,7 +6,26 @@ namespace Advanced2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Q1
+            String[] fristLine = Console.ReadLine().Split();
+            int n = int.Parse(fristLine[0]);
+            int q = int.Parse(fristLine[1]);
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            for (int i = 0; i < q; i++) { 
+            int x = int.Parse(Console.ReadLine());
+                int count = 0;
+                foreach (int num in arr) {
+                    if (num > x)
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+            }
+            #endregion
+            #region Q2
+            #endregion
         }
     }
 }
